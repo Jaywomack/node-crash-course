a620172e02a3b13a0d1864c4134ebc0127498f8d
jaywomack <womackjw@hotmail.com>
2021-01-01 15:48:15 -0800

    added a lot. Using express and mongoose to display data from the server with get put post delete

--- app.js
+++ app.js
@@ -1,27 +1,74 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+const { render } = require('ejs');
 
 // express app
 const app = express();
 
+// Connect to MongoDB
+const dbURI =
+  'mongodb+srv://womackjw:Fall$007@nodetuts.3husf.mongodb.net/node-tuts?retryWrites=true&w=majority';
+
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((
+    result // listen for requests
+  ) => app.listen(3000))
+  .catch((err) => console.log(err));
+
 // register view engine
 app.set('view engine', 'ejs');
 
-// listen for requests
-app.listen(3000);
+// Middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
+// routes
 app.get('/', (req, res) => {
-  const blogs = [
-    { title: 'Yoshi finds eggs', snippet: 'This is a snippet about a blog.' },
-    { title: 'Mario finds stars', snippet: 'This is a snippet about a blog.' },
-    {
-      title: 'How to defeat Bowser',
-      snippet: 'This is a snippet about a blog.',
-    },
-  ];
-  res.render('index', { title: 'Home', blogs });
+  res.redirect('/blogs');
 });
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
+});
+
+// Blog routes
+app.get('/blogs', (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render('index', { title: 'ALL BLOGS', blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog
+    .save()
+    .then((result) => {
+      res.redirect('/blogs');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  Blog.findById(id)
+    .then((result) => {
+      res.render('details', { blog: result, title: 'Blog Details' });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.get('/blogs/create', (req, res) => {
