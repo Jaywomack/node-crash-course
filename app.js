8707c895ede159157b88d838e710f027b981f2bc
jaywomack <womackjw@hotmail.com>
2020-12-31 20:39:17 -0800

    added a head nav and footer partial and styled the blog.

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+// express app
+const app = express();
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// listen for requests
+app.listen(3000);
+
+app.get('/', (req, res) => {
+  const blogs = [
+    { title: 'Yoshi finds eggs', snippet: 'This is a snippet about a blog.' },
+    { title: 'Mario finds stars', snippet: 'This is a snippet about a blog.' },
+    {
+      title: 'How to defeat Bowser',
+      snippet: 'This is a snippet about a blog.',
+    },
+  ];
+  res.render('index', { title: 'Home', blogs });
+});
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a New Blog' });
+});
+
+// 404 Page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
